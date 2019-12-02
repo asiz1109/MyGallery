b1fc5a4e52ee78cc13160f5f56eac415b4f3f74d
asiz1109 <anna-sizova1109@yandex.ru>
2019-12-02 18:19:36 +0300

    SquareImageView

---
+++ app/src/main/java/com/example/mygallery/SquareImageView.java
@@ -0,0 +1,19 @@
+package com.example.mygallery;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SquareImageView extends androidx.appcompat.widget.AppCompatImageView{
+
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
+    }
+}
